d4df64e347721737a18c8e8dde88e742106b5879
go2ismail <go2ismail@gmail.com>
2024-03-30 21:14:45 +0700

    new branch initialization

--- coderush/wwwroot/scripts/otherProducts.js
+++
@@ -1,2 +0,0 @@
-window.essentialJsUrl = "file:///C:/Users/Public/Documents/Syncfusion/JavaScript/16.2.0.41/JavaScript/samples/web/default.htm";
-window.mobileJsUrl = "file:///C:/Users/Public/Documents/Syncfusion/JavaScript/16.2.0.41/JavaScript/samples/mobile/desktop/default.htm";
